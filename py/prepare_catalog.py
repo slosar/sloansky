76d4c24ae062143b3273e7c0491ef4722ee15405
Anze Slosar <anze@bnl.gov>
2016-11-11 12:13:55 -0500

    parellized prepare catalog

--- py/prepare_catalog.py
+++ py/prepare_catalog.py
@@ -5,6 +5,7 @@ import pyfits
 import sys, cPickle
 import matplotlib.pyplot as plt
 from optparse import OptionParser
+from mpi4py import MPI
 
 dirname="sky_data/"
 outcat="catalog.cp"
@@ -20,22 +21,31 @@ parser.add_option("--skip", dest="skipfact", default=skipfact,
 ## add loglam options here
 
 (o, args) = parser.parse_args()
-
-globst=o.dirname+"/*/*.fits"
-print "Preparing file list from:",globst
-filelist=glob(globst)
+comm=MPI.COMM_WORLD
+rank=comm.Get_rank()
+size=comm.Get_size()
+if rank==0:
+    globst=o.dirname+"/*/*.fits"
+    print "Preparing file list from:",globst
+    filelist=glob(globst)
+else:
+    filelist=[]
+filelist=comm.bcast(filelist,root=0)
 Nf=len(filelist)
 Nfp=Nf/o.skipfact
-print "Done, we have ",Nf,"files, will use", Nfp,"."
+if (rank==0):
+    print "Done, we have ",Nf,"files, will use", Nfp,"."
+mystart=int(Nfp*rank*1.0/size)
+myend=int(Nfp*(rank+1)*1.0/size)
 
 if (Nf==0):
-    print "No files, quitting."
+    if rank==0:
+        print "No files, quitting."
     sys.exit(1)
     
 cat=[]
 totvars=[]
-for ifile,filename in enumerate(filelist[::o.skipfact]):
-    print "\rDoing %i / %i"%(ifile, Nfp),
+for ifile,filename in enumerate(filelist[mystart*o.skipfact:myend*o.skipfact:o.skipfact]):
     sys.stdout.flush()
     da=pyfits.open(filename)
     vars=[]
@@ -47,19 +57,25 @@ for ifile,filename in enumerate(filelist[::o.skipfact]):
         vars.append(var)
         totvars.append(var)
     cat.append((filename, np.array(vars)))
-print 
+allcats=comm.gather(cat,root=0)
+alltotvars=comm.gather(totvars,root=0)
+if rank==0:
+# adding cats from nodes 1 onward to root cat
+    for subcat in allcats[1:]:
+        cat+=subcat
+    for subtotvars in alltotvars[1:]:
+        totvars+=subtotvars
+    totvars=np.array(totvars)
+    minv=totvars.min(axis=0)
+    maxv=totvars.max(axis=0)
+    meanv=totvars.mean(axis=0)
+    medv=np.median(totvars,axis=0)
 
-totvars=np.array(totvars)
-minv=totvars.min(axis=0)
-maxv=totvars.max(axis=0)
-meanv=totvars.mean(axis=0)
-med=np.median(totvars,axis=0)
+    for i,mn,mx,mean,med in zip(range(len(minv)),minv,maxv,meanv,medv):
+        print "Variable %i: min, max, mean, median: %f %f %f %f"%(i,
+             mn,mx,mean,med)
 
-for i,mn,mx,mean,med in zip(range(len(minv)),minv,maxv,meanv,med):
-    print "Variable %i: min, max, mean, median: %f %f %f %f"%(i,
-         mn,mx,mean,med)
-
-cPickle.dump((cat,(mn,mx,mean,med)),open(o.outcat,'w'))
+    cPickle.dump((cat,(minv,maxv,meanv,medv)),open(o.outcat,'w'))
 
 
 
